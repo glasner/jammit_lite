a2537761fc9874cc10669af93d7131ede52569c9
Jordan Glasner <jordan@digitalignition.com>
2010-03-29 11:05:58 -0400

    refactored into smaller methods

--- lib/jammit_lite.rb
+++ lib/jammit_lite.rb
@@ -4,26 +4,33 @@ module Jammit
     
     # returns hash of bundle file arrays keyed by bundle name
     def self.assets(type)
-      hash = asset_hash[type].symbolize_keys
-      assets = hash.inject({}) do |out,array|   
+      hash = asset_hash(type)
+      hash_to_assets(hash)
+    end
+    
+    # typecasts paths in given Hash into Assets
+    def self.hash_to_assets(hash)
+      hash.inject({}) do |out,array|   
         key,paths = array
         out[key] = paths.map { |path| Jammit::Lite::Asset.new(path) }
         out
       end
-    end
+    end    
     
-    
-    # returns hash from config/assets.yml
+    # returns hash from yaml with given type, e.g. :javascripts
     # expands all wildcard paths
-    def self.asset_hash
-      yaml.each do |key,bundles|
-        bundles.each { |name,files| bundles[name] = Jammit::Lite::Bundle.expand(files) }
+    def self.asset_hash(type)
+      yaml[type].symbolize_keys.inject({}) do |out,pair|
+        bundle,files = pair
+        out[bundle] = Jammit::Lite::Bundle.expand(files)
+        out
       end
     end
     
+    # returns hash from config/assets.yml
     def self.yaml
       yaml = File.open("#{Rails.root}/config/assets.yml")
-      hash = YAML::load(yaml).symbolize_keys
+      YAML::load(yaml).symbolize_keys
     end
     
   end
